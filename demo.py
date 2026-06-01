139bc07d099f921f6366f00cf928ad8db6a07b91
workintechpoyrazaka-sketch <workintech.poyrazaka@gmail.com>
2026-06-01 13:05:53 +0300

    Solve circle_area: area formula with negative-radius guard

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
-
+import math
 
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    if radius < 0:
+        return 0
+    return math.pi * radius ** 2
